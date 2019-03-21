35272e847d4076798e8046cb44db5f2734693bbe
itggot-linnea-nordqvist <linnea.nordqvist@elev.ga.ntig.se>
2019-03-21 15:34:41 +0100

    grid till kategori + produkter

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -1,5 +1,7 @@
 function menu(event){
     let menu=document.querySelector("nav")
     menu.classList.toggle("test")
+    let blurred=document.querySelector(".wrapper")
+    blurred.classList.toggle("blurred")
     
 }
